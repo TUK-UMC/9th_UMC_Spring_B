a84455f90f9dabacd66fd1409deecbced51ce002
정지훈 <jjh00251@gmail.com>
2025-10-29 22:50:29 +0900

    5주차 과제 제출

---
+++ cookie/chapter4/src/main/java/com/example/chapter4/domain/Alarm/entity/Alarm.java
@@ -0,0 +1,17 @@
+package com.example.chapter4.domain.Alarm.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "alrim")
+public class Alarm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long memberId;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
